bacda5771a56e1925cdebf9061eb2ccdafbae952
Parker Hodge <iparkerhodge@gmail.com>
2020-04-29 09:25:52 -0500

    added login, registration, and mock user authentication

--- src/components/navigation/TopNav.js
+++ src/components/navigation/TopNav.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './Navigation.css'
 import {ReactComponent as MenuButton} from '../../images/Menu Button.svg'
 
-const TopNav = ({toggle}) => (
+const TopNav = ({toggleNav}) => (
     <header className="topNav">
-        <div className="menuButtonContainer"><MenuButton className="menuButton" onClick={toggle}/></div>
+        <div className="menuButtonContainer"><MenuButton className="menuButton" onClick={toggleNav}/></div>
         Welcome to Custom Covid Tracker
     </header>
 )
