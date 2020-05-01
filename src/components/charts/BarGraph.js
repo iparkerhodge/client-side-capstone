42dd9003e567652c023f32c2fe12941eaa06fb46
Parker Hodge <iparkerhodge@gmail.com>
2020-05-01 12:54:39 -0500

    parsed data into object for each date with date,cases keys

--- src/components/charts/BarGraph.js
+++ src/components/charts/BarGraph.js
@@ -1,9 +1,43 @@
 import React, { useContext } from 'react'
-import { select } from 'd3'
+import { select, selectAll } from 'd3'
 import { DataContext } from '../data/DataProvider'
+import { DateContext } from '../data/DateCalculator'
 
 export const BarGraph = () => {
     const { timeSeriesGlobal } = useContext(DataContext)
+    const { allDateArray } = useContext(DateContext)
+
+    //get global total for each day
+    const arrayOfTotalsForEachDay = []
+
+    //write a function that takes input date and pushes the global total to array
+    const totalForDate = (date) => {
+        let total = 0
+        timeSeriesGlobal.map(prov => {
+            return total += +(prov[date])
+        })
+        arrayOfTotalsForEachDay.push(total)
+    }
+
+    //map date array and convert each date to a total
+    allDateArray.map(date => totalForDate(date))
+
+    //turn into object {date:'4/30/20, cases:'2000000'}
+    const data = []
+    allDateArray.forEach((v,i) => {
+        const obj = {}
+        obj.date = v
+        obj.cases = arrayOfTotalsForEachDay[i]
+        data.push(obj)
+    })
+
+    console.log(data)
+
+    //render bar chart
+    const render = () => {
+        svg.selectAll('rect')
+
+    }
 
     const svg = select('svg')
     const width = +svg.attr('width')
