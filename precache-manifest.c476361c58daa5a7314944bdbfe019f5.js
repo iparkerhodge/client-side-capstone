32711e255113824b5f62bfc9e435e89fd1ef9b66
Parker Hodge <iparkerhodge@gmail.com>
2020-05-26 20:11:46 -0500

    Updates

---
+++ precache-manifest.c476361c58daa5a7314944bdbfe019f5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548f67e79f2b8b45200e038c9d8dabfd",
+    "url": "/client-side-capstone/index.html"
+  },
+  {
+    "revision": "8b9d15584f46e34d8461",
+    "url": "/client-side-capstone/static/css/2.dafc00e9.chunk.css"
+  },
+  {
+    "revision": "7b4d0e926e54652f020c",
+    "url": "/client-side-capstone/static/css/main.110df746.chunk.css"
+  },
+  {
+    "revision": "8b9d15584f46e34d8461",
+    "url": "/client-side-capstone/static/js/2.c5df3178.chunk.js"
+  },
+  {
+    "revision": "a8c6e8e78f131de93523f355224b8411",
+    "url": "/client-side-capstone/static/js/2.c5df3178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b4d0e926e54652f020c",
+    "url": "/client-side-capstone/static/js/main.6f6d7816.chunk.js"
+  },
+  {
+    "revision": "8fe748e7b90b2130a6a4",
+    "url": "/client-side-capstone/static/js/runtime-main.51172c17.js"
+  },
+  {
+    "revision": "9298788641b6c237a845f8210d93c6e6",
+    "url": "/client-side-capstone/static/media/Dashboard.92987886.svg"
+  },
+  {
+    "revision": "974b22a249c7d9d15af8ed9af4acc4a0",
+    "url": "/client-side-capstone/static/media/Menu Button.974b22a2.svg"
+  },
+  {
+    "revision": "88f2ea7c24bcbe33e09ef19b734924ba",
+    "url": "/client-side-capstone/static/media/arrow.88f2ea7c.svg"
+  },
+  {
+    "revision": "9f6b6109264ed1393eb11bfecda98b91",
+    "url": "/client-side-capstone/static/media/charts.9f6b6109.svg"
+  },
+  {
+    "revision": "460c1c4e9f09311d511d770a12f4b4db",
+    "url": "/client-side-capstone/static/media/world.460c1c4e.svg"
+  }
+]);
